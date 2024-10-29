c30bb01b9b98740d79971de2b5ef2b30e8c29eb0
Peter Zerbinos <peter.zerbinos@mths.ca>
2024-10-29 11:24:49 -0400

    created neopixel program

--- main.ts
+++ main.ts
@@ -1,8 +1,67 @@
-/* Copyright (c) 2020 MTHS All rights reserved
+/* Copyright (c) 2024 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
+ * Created by: Peter Zerbinos
+ * Created on: Oct 2024
+ * This program counts down when you press the "A" or "B" button
 */
 
-basic.showString('Hello, World!')
+//variable
+let count: number
+let neopixelStrip: neopixel.Strip = null
+
+//clear neopixel (setup)
+basic.clearScreen()
+neopixelStrip = neopixel.create(DigitalPin.P16, 4, NeoPixelMode.RGB)
+neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Black))
+neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Black))
+neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))
+neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Black))
+neopixelStrip.show()
+basic.showIcon(IconNames.Happy)
+
+//countdown from 4 to 0
+input.onButtonPressed(Button.A, function() {
+    count = 4
+    while (count > 0){
+        basic.clearScreen()
+        if (count = 4) {
+            neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.White))
+            neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Black))
+            neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))
+            neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Black))
+            neopixelStrip.show()
+            basic.showNumber(count)
+            basic.pause(1000)
+        }
+        if (count = 3) {
+            neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Black))
+            neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.White))
+            neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))
+            neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Black))
+            neopixelStrip.show()
+            basic.showNumber(count)
+            basic.pause(1000)
+        }
+        if (count = 2) {
+            neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Black))
+            neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Black))
+            neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.White))
+            neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Black))
+            neopixelStrip.show()
+            basic.showNumber(count)
+            basic.pause(1000)
+        }
+        if (count = 1) {
+            neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Black))
+            neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Black))
+            neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))
+            neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.White))
+            neopixelStrip.show()
+            basic.showNumber(count)
+            basic.pause(1000)
+        count = 0
+        neopixelStrip.clear()
+        basic.showIcon(IconNames.Happy)
+        }
+    }
+})
